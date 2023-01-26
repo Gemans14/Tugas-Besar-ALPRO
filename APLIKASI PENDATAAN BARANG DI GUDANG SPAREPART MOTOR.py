cd14e073310d9de210e73add1ad76d25eaa0e745
Gemans14 <gemasakti31@gmail.com>
2023-01-26 14:48:14 +0700

    APLIKASI PENDATAAN BARANG

--- APLIKASI PENDATAAN BARANG DI GUDANG SPAREPART MOTOR.py
+++ APLIKASI PENDATAAN BARANG DI GUDANG SPAREPART MOTOR.py
@@ -1,31 +1,30 @@
-from os import system
-     
-if __name__ == "__main__":
-    list_barang = []
+list_Sparepart = ["Roller Motor", "Klakson Denso", "Per CVT"]
+list_harga = [100000, 150000, 75000]
+list_stok = [1, 2, 3,]
 
-    while True:
-        system('clear') #cls
-        
-        print("="*40)
-        print("Aplikasi Pendataan Gudang Sparepart Motor : JAYA MOTOR")
-        print("="*40)
+def LihatBarang ():
+    print("Daftar Barang")
+    menu = zip(list_Sparepart, list_harga, list_stok)
+    print ("{:<5}| {:<20} | {:<12} | {:<5} |".format("No", "Nama Barang", "Harga", "Stok"))
+    for index, y in enumerate (menu):
+        Nama, Harga, Stok = y
+        print ("{:<5}| {:<20} | {:<12} | {:<5} |".format( index+1, Nama, Harga, Stok))
 
-        print("-"*20)
-        print("Pilihan Menu:")
-        print("1. Lihat Barang")
-        print("2. Input Barang")
-        print("0. Keluar")
-        print("-"*20)
-        menu = ("menu: ")
+list_tampilan = ["1. Lihat Barang", "2. Input Barang", "3. Update Barang", "4. Hapus Barang"]
 
-        if menu == "0":
-            break
-        elif menu == "1":           
-                print("Lihat Barang")
-
-        elif menu == "2":
-            print("Input Barang")       
-        else:
-            print("Hanya boleh memilih menu 0, 1, atau 2")
-
-        input("-- press any key to continue --")
+while True :
+    print("Pilihan Menu:")
+    for x in list_tampilan:
+        print(x)
+    print("Press any key to continue")
+    menu = int(input())
+    if menu == 1:
+        LihatBarang ()
+    elif menu == 2:
+        MasukanBarang ()
+    elif menu == 3:
+        UbahBarang ()
+    elif menu == 4:
+        HapusBarang ()
+    else:
+        break
